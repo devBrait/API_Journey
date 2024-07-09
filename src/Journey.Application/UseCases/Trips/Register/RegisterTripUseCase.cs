50bee4054d24ef2852ecb1b55c746a89396cff4a
brait <lorimiervb@gmail.com>
2024-07-09 19:23:29 -0300

    update files

---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,55 @@
+﻿using Journey.Communication.Requests;
+using Journey.Communication.Responses;
+using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
+using Journey.Infrastructure;
+using Journey.Infrastructure.Entities;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripUseCase
+    {
+        public ResponseShortTripJson Execute(RequestRegisterTripJson request)
+        {
+            Validate(request);
+
+            var dbContext = new JourneyDbContext();
+
+            var entity = new Trip
+            {
+                Name = request.Name,
+                StartDate = request.StartDate,
+                EndDate = request.EndDate,
+            };
+
+            dbContext.Trips.Add(entity);
+            dbContext.SaveChanges();
+
+            return new ResponseShortTripJson
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                EndDate = entity.EndDate,
+                StartDate = entity.StartDate,
+            };
+        }
+
+        private void Validate(RequestRegisterTripJson request)
+        {
+            if (string.IsNullOrEmpty(request.Name))
+            {
+                throw new JourneyException(ResourceErrorMessages.NameEmpty);
+            }
+
+            if (request.StartDate.Date < DateTime.UtcNow.Date)
+            {
+                throw new JourneyException(ResourceErrorMessages.DateStartTrip);
+            }
+
+            if (request.EndDate.Date < request.StartDate.Date)
+            {
+                throw new JourneyException(ResourceErrorMessages.DateEndTrip);
+            }
+        }
+    }
+}
